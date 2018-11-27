7840be616fc5a01cc2615a83270a14e0ac72ffa9
mikaelra <42651436+mikaelra@users.noreply.github.com>
2018-11-27 14:53:26 +0100

    added py files and images

---
+++ RandomForest.py
@@ -0,0 +1,175 @@
+import sys
+import warnings
+
+if not sys.warnoptions:
+    warnings.simplefilter("ignore")
+
+import numpy as np
+import pandas as pd
+import re
+import tensorflow as tf
+from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
+import random
+from sklearn.metrics import r2_score, mean_squared_error, accuracy_score, log_loss
+import matplotlib.pyplot as plt
+
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.tree import DecisionTreeClassifier
+
+def easydatagen():
+    # Reading in the training file
+    data = pd.read_json('train.json')
+    """
+    print("An example of what the data looks like:")
+    print("")
+    print(data)
+
+    print("")
+    print("Here the number of recepies of each cuisine")
+    print("")
+    print(data['cuisine'].value_counts())
+    """
+    # The set of different cuisines
+    cuisines = data.cuisine.unique()
+
+    # To find the different ingredients, we need to clean them up a little.
+    def clean(string) :
+        s = string.replace('-',' ') # read low-fat the same as low fat
+        s = string.replace('&', 'and') # read & and and as the same
+        s = re.sub('\((.*?)\)', '', s) # remove everythin g in brackets
+        s = re.sub('\d{1,2}\%', '', s) # remove things of the form d% or dd%, where d is a digit
+        s = ' '.join(s.split()) # remove extra white spaces
+
+        return s
+
+    ing_list = data.ingredients.values.tolist()
+    raw_ingredients = [clean(x) for ing in ing_list for x in ing]
+
+    ingredients = sorted(set(raw_ingredients))
+    """
+    print("There are %d different ingredients." % len(ingredients))
+    print("")
+    print("")
+    print("Here is the very long list:")
+    print("")
+    print(ingredients)
+    """
+    # build a dictionary that to each ingredient assigns its index
+    ingredient_index = {}
+    for i in range(0,len(ingredients)) :
+        ingredient_index[ingredients[i]] = i
+
+    # the same for cuisines
+    cuisine_index = {}
+    for i in range(0, len(cuisines)) :
+        cuisine_index[cuisines[i]] = i
+
+    def ingredients_to_vector(ings) :
+        vect = np.zeros(len(ingredients))
+        for ing in ings :
+            vect[ingredient_index[clean(ing)]] = 1
+
+        return vect
+
+    def cuisine_to_vector(cus) :
+        vect = np.zeros(20)
+        vect[cuisine_index[cus]] = 1
+        return vect
+
+    vect_list = [ingredients_to_vector(ing) for ing in ing_list]
+    target_list = [cuisine_to_vector(cus) for cus in data.cuisine.values.tolist()]
+    """
+    print(len(vect_list))
+    print(len(target_list))
+
+    print(vect_list[30064])
+    print(target_list[30064])
+    """
+
+    # Define training data
+    X = np.c_[vect_list]
+    Y = np.c_[target_list]
+
+    #print('Shape of X: ' + str(X.shape))
+    #print('Shape of Y: ' + str(Y.shape))
+
+    Y_num = np.zeros((Y.shape[0]))
+    for i in range(Y.shape[0]):
+        Y_num[i] = np.argmax(Y[i])
+
+    x_train, x_test, y_train, y_test = train_test_split(X, Y_num, test_size = 0.2)
+
+    return x_train, x_test, y_train, y_test
+
+if __name__ == '__main__':
+
+    x_train, x_test, y_train, y_test = easydatagen()
+    """
+    #
+    # Let's start making forests
+    #
+    """
+
+    """
+    # Makes 31 random forests with 1 to 32 trees
+    # Plots the accuracies
+
+    print('Starting training:')
+
+    testscores = []
+    trainscores = []
+    index = []
+
+    for i in range(1,32):
+        clf = RandomForestClassifier(n_estimators=i, max_depth=None, max_features='auto',
+                                 verbose=True, n_jobs=8)
+        clf.fit(x_train, y_train)
+
+        index.append(i)
+        testscores.append(clf.score(x_test, y_test))
+        trainscores.append(clf.score(x_train, y_train))
+
+    plt.plot(index, testscores, '-b' , label='Testing data')
+    plt.plot(index, trainscores, '-r', label='Training data')
+    plt.legend(loc='center right')
+    plt.title('Accuracy of random forests')
+    plt.xlabel('Trees')
+    plt.ylabel('Accuracy')
+    plt.show()
+    """
+
+    """
+    # Plot tells us that we can use 10 trees to get a decent score
+    """
+
+    """
+    print('Starting training:')
+
+    testscores = []
+    trainscores = []
+    index = []
+
+    """
+    # Auto = sqrt(classifiers) = 81     log2 = 12
+    # Use then different values as max_features
+    """
+    for i in range(1, 200, 10):
+        clf = RandomForestClassifier(n_estimators=10, max_depth=None, max_features= i,
+                                 verbose=True, n_jobs=8)
+        clf.fit(x_train, y_train)
+
+        index.append(i)
+        testscores.append(clf.score(x_test, y_test))
+        trainscores.append(clf.score(x_train, y_train))
+
+    plt.plot(index, testscores, '-b' , label='Testing data')
+    plt.plot(index, trainscores, '-r', label='Training data')
+    plt.legend(loc='center right')
+    plt.title('Accuracy of random forests')
+    plt.xlabel('Max_features')
+    plt.ylabel('Accuracy')
+    plt.show()
+
+    # Nothing conclusive
+    """
---
+++ RandomForestPCA.py
@@ -0,0 +1,106 @@
+# This code classifies the data differently
+# https://www.kaggle.com/gloriahristova/a-walkthrough-eda-vizualizations-unigram-model/notebook
+
+# Data processing
+import pandas as pd
+import json
+from collections import Counter
+from itertools import chain
+from sklearn.feature_extraction.text import TfidfVectorizer
+import numpy as np
+import re
+
+# Data vizualizations
+import matplotlib.pyplot as plt
+
+# Data Modeling
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.preprocessing import LabelEncoder
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import SVC
+from sklearn.ensemble import VotingClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import KFold
+from sklearn import model_selection
+from sklearn.model_selection import train_test_split
+import warnings
+warnings.filterwarnings('ignore')
+
+# Import own classified data
+from RandomForest import *
+
+train_data = pd.read_json('train.json') # store as dataframe objects
+
+# Prepare the data
+features = [] # list of list containg the recipes
+for item in train_data['ingredients']:
+    features.append(item)
+
+ingredients = [] # this list stores all the ingredients in all recipes (with duplicates)
+for item in train_data['ingredients']:
+    for ingr in item:
+        ingredients.append(ingr)
+
+target = train_data['cuisine']
+
+# Both train and test samples are processed in the exact same way
+# Train
+features_processed= [] # here we will store the preprocessed training features
+for item in features:
+    newitem = []
+    for ingr in item:
+        ingr.lower() # Case Normalization - convert all to lower case
+        ingr = re.sub("[^a-zA-Z]"," ",ingr) # Remove punctuation, digits or special characters
+        ingr = re.sub((r'\b(oz|ounc|ounce|pound|lb|inch|inches|kg|to)\b'), ' ', ingr) # Remove different units
+        newitem.append(ingr)
+    features_processed.append(newitem)
+
+#Binary representation of the training set will be employed
+vectorizer = CountVectorizer(analyzer = "word",
+                             ngram_range = (1,1), # unigrams
+                             binary = True, #  (the default is counts)
+                             tokenizer = None,
+                             preprocessor = None,
+                             stop_words = None,
+                             max_df = 0.99) # any word appearing in more than 99% of the sample will be discarded
+
+# Fit the vectorizer on the training data and transform the test sample
+X = vectorizer.fit_transform([str(i) for i in features_processed])
+
+# Apply label encoding on the target variable (before model development)
+lb = LabelEncoder()
+Y = lb.fit_transform(target)
+
+#split data
+x_train1, x_test1, y_train1, y_test1 = train_test_split(X, Y, test_size = 0.2)
+
+x_train2, x_test2, y_train2, y_test2 = easydatagen()
+
+# Makes 31 random forests with 1 to 32 trees
+# Plots the accuracies
+
+print('Starting training:')
+
+testscores1 = []
+testscores2 = []
+index = []
+
+for i in range(1,32):
+    clf1 = RandomForestClassifier(n_estimators=i, max_depth=None, max_features='auto',
+                             verbose=True, n_jobs=8)
+    clf1.fit(x_train1, y_train1)
+    clf2 = RandomForestClassifier(n_estimators=i, max_depth=None, max_features='auto',
+                             verbose=True, n_jobs=8)
+    clf2.fit(x_train2, y_train2)
+
+    index.append(i)
+    testscores1.append(clf1.score(x_test1, y_test1))
+    testscores2.append(clf2.score(x_test2, y_test2))
+
+plt.plot(index, testscores1, '-b' , label=' "PCA"-test data')
+plt.plot(index, testscores2, '-r', label='Own test data')
+plt.legend(loc='center right')
+plt.title('Accuracy of random forests')
+plt.xlabel('Trees')
+plt.ylabel('Accuracy')
+plt.show()
