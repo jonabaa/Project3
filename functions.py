3de59017faec24e4093f9e3f80ea8c883f364245
jeanpylon <jonaudunbaar@gmail.com>
2018-11-28 14:09:52 +0100

    add file functions.py

---
+++ functions.py
@@ -0,0 +1,32 @@
+# Functions for project 3
+
+
+
+# Adam, implement this
+def generate_design_matrix():
+    design_matrix = None
+
+    return design_matrix
+
+
+# Jon, implement this
+def best_accuracy_log_reg(x_train, y_train, x_test, y_test, param_list):
+    best_accuracy = 0
+
+    return best_accuracy
+
+
+# Adam, implement this
+def best_accuracy_svm(x_train, y_train, x_test, y_test, param_list):
+    best_accuracy = 0
+
+    return best_accuracy
+
+
+# Mikael, implement this
+def best_accuracy_forests(x_train, y_train, x_test, y_test, param_list):
+    best_accuracy = 0
+
+    return best_accuracy
+
+
